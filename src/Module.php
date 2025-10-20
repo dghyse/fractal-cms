cedcbb0d64f8502dbb3f43e32fb99791a69657bd
David Ghyse <dghyseredcat.fr>
2025-10-20 16:15:35 +0200

    up to version 1.5.0, update CHAGELOG.md

--- src/Module.php
+++ src/Module.php
@@ -42,7 +42,7 @@ class Module extends \yii\base\Module implements BootstrapInterface
     public $relativeItemImgDirName = 'items';
     public $relativeSeoImgDirName = 'seo';
     public $cacheImgPath = 'cache';
-    public $version = 'v1.4.0';
+    public $version = 'v1.5.0';
     public $name = 'FractalCMS';
     public $commandNameSpace = 'fractalCms:';
 
